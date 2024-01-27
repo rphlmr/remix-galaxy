9640ab1c72abc736f62d090347a86b86ea25065b
rphlmr <raphael@uaerom.com>
2024-01-27 18:43:57 +0100

    📦 NEW: Remix Hono Vite playground

---
+++ playgrounds/remix-hono-vite/app/root.tsx
@@ -0,0 +1,32 @@
+import {
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from "@remix-run/react";
+// https://remix.run/docs/en/main/future/vite#fix-up-css-imports
+import "~/tailwind.css";
+
+export default function App() {
+	return (
+		<html lang="en" className="h-dvh w-screen bg-zinc-900 text-zinc-50">
+			<head>
+				<meta charSet="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1.0,maximum-scale=1.0,viewport-fit=cover"
+				/>
+				<Meta />
+				<Links />
+			</head>
+			<body className="size-full">
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
+}
---
+++ playgrounds/remix-hono-vite/app/routes/_index.tsx
@@ -0,0 +1,62 @@
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const meta: MetaFunction = () => [
+	{ title: "New Remix App" },
+	{ name: "description", content: "Welcome to Remix!" },
+];
+
+export function loader({ context }: LoaderFunctionArgs) {
+	const { appVersion } = context;
+	const message = "Hello World from Remix Vite loader";
+	console.log(message, appVersion);
+	return { message, appVersion };
+}
+
+export default function Index() {
+	const { message, appVersion } = useLoaderData<typeof loader>();
+	return (
+		<div>
+			<h1 className="text-3xl font-bold text-purple-500">
+				Welcome to Remix Vite !
+			</h1>
+			<label>
+				Should persist state across
+				<input type="text" placeholder="HMR test" />
+			</label>
+			<ul>
+				<li>
+					<a
+						target="_blank"
+						href="https://remix.run/tutorials/blog"
+						rel="noreferrer"
+						className="text-blue-500"
+					>
+						15m Quickstart Blog Tutorial
+					</a>
+				</li>
+				<li>
+					<a
+						target="_blank"
+						href="https://remix.run/tutorials/jokes"
+						rel="noreferrer"
+					>
+						Deep Dive Jokes App Tutorial
+					</a>
+				</li>
+				<li>
+					<a
+						target="_blank"
+						href="https://remix.run/docs"
+						rel="noreferrer"
+					>
+						Remix Docs
+					</a>
+				</li>
+			</ul>
+
+			<div>{message}</div>
+			<div>appVersion: {appVersion}</div>
+		</div>
+	);
+}
