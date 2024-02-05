5ecfd7d8b8b7b8512d97d91e70530f813c0c747f
rphlmr <raphael@uaerom.com>
2024-02-05 08:43:06 +0100

    🐛 FIX: fix cache for defer @huijiewei

--- playgrounds/remix-hono/server/middlewares.ts
+++ playgrounds/remix-hono/server/middlewares.ts
@@ -103,9 +103,17 @@ export function refreshSession() {
  *
  * @param seconds - The number of seconds to cache
  */
-export function cache(seconds: number): MiddlewareHandler {
+export function cache(seconds: number) {
 	return createMiddleware(async (c, next) => {
+		if (!c.req.path.match(/\.[a-zA-Z0-9]+$/)) {
+			return next();
+		}
+
 		await next();
+
+		if (!c.res.ok) {
+			return;
+		}
 
 		c.res.headers.set("cache-control", `public, max-age=${seconds}`);
 	});
