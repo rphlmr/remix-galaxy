73d84acca7930faac047a15171db950da99833ae
rphlmr <raphael@uaerom.com>
2024-02-22 09:48:32 +0100

    🐛 FIX: remix vite docker build

---
+++ playgrounds/remix-hono-vite/app/entry.client.tsx
@@ -0,0 +1,13 @@
+import { startTransition, StrictMode } from "react";
+
+import { RemixBrowser } from "@remix-run/react";
+import { hydrateRoot } from "react-dom/client";
+
+startTransition(() => {
+	hydrateRoot(
+		document,
+		<StrictMode>
+			<RemixBrowser />
+		</StrictMode>,
+	);
+});
